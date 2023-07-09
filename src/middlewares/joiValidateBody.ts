c1d9a1b7fc74f6000af07e9a2d0f50c43ae149e5
VolodymyrSkurikhin <vladiskuri@gmail.com>
2023-07-09 18:57:20 +0300

    fix validation with {}

--- src/middlewares/joiValidateBody.ts
+++ src/middlewares/joiValidateBody.ts
@@ -2,7 +2,7 @@ import { HttpError } from "../helpers/HttpError.js";
 
 export const joiValidateBody = (schema) => {
   const func = (req, _res, next) => {
-    const err = schema.validate(req.body);
+    const { err } = schema.validate(req.body);
     if (err) {
       next(HttpError(400, err.message));
     }
